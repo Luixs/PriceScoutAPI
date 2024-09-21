4a145f8b25a29a4006a60278b1ed34f6ace16544
Luis Starlino <luis.starlino@arcalis.com.br>
2024-09-21 08:49:33 -0300

    Before performing the advanced search, editing some unit tests.

---
+++ PriceScoutAPI.Tests/SearchTests.cs
@@ -0,0 +1,27 @@
+﻿using FakeItEasy;
+using PriceScoutAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceScoutAPI.Tests
+{
+    public class SearchTests
+    {
+        private AmazonHelper _amazonHelper;
+
+        public SearchTests()
+        {
+            // --- Dependencies
+            _amazonHelper = A.Fake<AmazonHelper>();
+        }
+
+        [Fact]
+        public void AmazonHelper_FindPrices_ReturnsTaskModel()
+        {
+
+        }
+    }
+}
