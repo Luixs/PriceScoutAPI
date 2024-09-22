b1f756d8ec52238620a2fc7df6d8a525eacdd50f
Luis Starlino <luis.starlino@arcalis.com.br>
2024-09-22 09:22:19 -0300

    Completed the first unit test

--- PriceScoutAPI.Tests/SearchTests.cs
+++ PriceScoutAPI.Tests/SearchTests.cs
@@ -1,6 +1,7 @@
 ﻿using FakeItEasy;
 using FluentAssertions;
 using PriceScoutAPI.Helpers;
+using PriceScoutAPI.Interfaces;
 using PriceScoutAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -12,17 +13,36 @@ namespace PriceScoutAPI.Tests
 {
     public class SearchTests
     {
+        private IBestOptionHelper _iBestOptionHelper;
         private AmazonHelper _amazonHelper;
         private SearchModel _searchModel;
+        private ProductModel _productModel;
 
         public SearchTests()
         {
             // --- Dependencies
+            _iBestOptionHelper = A.Fake<IBestOptionHelper>();
             _amazonHelper = A.Fake<AmazonHelper>();
             _searchModel = new SearchModel();
+            _productModel = new ProductModel();
         }
 
         [Fact]
+        public void IBestOptionHelper_ChooseBestOption_ReturnsProductModel()
+        {
+            // --- Arrange | What do i need to bring in here?
+            var fakeListProducts = A.Fake<List<ProductModel>>();
+            A.CallTo(()=> _iBestOptionHelper.ChooseBestOption(fakeListProducts)).Returns(_productModel);
+
+            // --- Act
+            var result = _iBestOptionHelper.ChooseBestOption(fakeListProducts);
+
+            // --- Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ProductModel>();
+        }
+
+        //[Fact]
         public void AmazonHelper_FindPrices_ReturnsTaskModel()
         {
             // --- Arrange | What do i need to bring in?
